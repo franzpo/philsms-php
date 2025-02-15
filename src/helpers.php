1f0995064fff1fff465e052a80400789fb23ddfc
JayDoesPHP <amarigondon@forbescollege.org>
2025-02-15 13:19:14 +0800

    Improve phone number formatting function to handle various input formats and return false for invalid numbers

--- src/helpers.php
+++ src/helpers.php
@@ -3,11 +3,17 @@
 if (!function_exists('formatPhoneNumber')) {
     function formatPhoneNumber($number)
     {
-        return match (strlen($number)) {
-            10 => "63" . $number,
-            11 => "63" . substr($number, 2),
-            13 => substr($number, 1),
-            default => $number,
-        };
+        $number = preg_replace('/\D/', '', $number);
+
+        if (substr($number, 0, 2) === '63' && strlen($number) === 12) {
+            return '0' . substr($number, 2);
+        }
+        if (strlen($number) === 10 && $number[0] === '9') {
+            return '0' . $number;
+        }
+        if (strlen($number) === 11 && $number[0] === '0') {
+            return $number;
+        }
+        return false;
     }
 }
